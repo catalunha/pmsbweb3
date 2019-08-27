16c69d1c9638aa5f948fe4093aad70730c2f7665
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-08-27 19:04:57 -0300

    usa authBloc instanciado em bootstrap

--- lib/components/default_scaffold.dart
+++ lib/components/default_scaffold.dart
@@ -15,11 +15,9 @@ class Rota {
 }
 
 class DefaultDrawer extends StatelessWidget {
-
   final AuthBloc authBloc;
   Map<String, Rota> rotas;
-  DefaultDrawer()
-      : authBloc = AuthBloc(AuthApiMobile(), Bootstrap.instance.firestore) {
+  DefaultDrawer() : authBloc = Bootstrap.instance.authBloc {
     // Map<String, Rota>
     rotas = Map<String, Rota>();
     rotas["/desenvolvimento"] = Rota("Desenvolvimento", Icons.build);
@@ -40,139 +38,141 @@ class DefaultDrawer extends StatelessWidget {
     // var authBloc = Provider.of<AuthBloc>(context);
     print("k >> teste");
     return Drawer(
-        child: SafeArea(
-      child: Column(
+      child: SafeArea(
+        child: Column(
           // padding: EdgeInsets.zero,
           children: <Widget>[
-            // StreamBuilder<UsuarioModel>(
-            //   stream: authBloc.perfil,
-            //   builder: (context, snap) {
-            //     if (snap.hasError) {
-            //       return Center(
-            //         child: Text("Erro"),
-            //       );
-            //     }
-            //     if (!snap.hasData)
-            //       return Center(
-            //         child: CircularProgressIndicator(),
-            //       );
-            //     Widget imagem = Icon(Icons.people, size: 75);
-            //     if (snap.data?.foto?.localPath != null) {
-            //       imagem = Container(
-            //           color: Colors.yellow,
-            //           child: Padding(
-            //               padding: const EdgeInsets.all(2.0),
-            //               child: CircleAvatar(
-            //                 backgroundImage:
-            //                     ExactAssetImage(snap.data?.foto?.localPath),
-            //                 minRadius: 50,
-            //                 maxRadius: 50,
-            //               )));
-            //     } else if (snap.data?.foto?.url != null) {
-            //       imagem = CircleAvatar(
-            //         backgroundImage: NetworkImage(snap.data?.foto?.url),
-            //         minRadius: 50,
-            //         maxRadius: 50,
-            //       );
-            //     }
-            //     return DrawerHeader(
-            //       child: Container(
-            //         decoration: BoxDecoration(
-            //           border: Border(
-            //             bottom: BorderSide(
-            //                 color: Theme.of(context).textTheme.title.color),
-            //           ),
-            //         ),
-            //         child: Column(
-            //           crossAxisAlignment: CrossAxisAlignment.start,
-            //           children: <Widget>[
-            //             Row(
-            //               crossAxisAlignment: CrossAxisAlignment.start,
-            //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //               children: <Widget>[
-            //                 Expanded(flex: 4, child: imagem),
-            //                 Expanded(
-            //                   flex: 8,
-            //                   child: Container(
-            //                     padding: EdgeInsets.only(left: 12),
-            //                     child: Column(
-            //                       crossAxisAlignment: CrossAxisAlignment.start,
-            //                       children: <Widget>[
-            //                         Padding(
-            //                           padding: const EdgeInsets.only(top: 8),
-            //                           child: Text("${snap.data.nome}"),
-            //                         ),
-            //                         Padding(
-            //                           padding: const EdgeInsets.only(top: 8),
-            //                           child: Text("${snap.data.celular}"),
-            //                         ),
-            //                       ],
-            //                     ),
-            //                   ),
-            //                 ),
-            //               ],
-            //             ),
-            //             Padding(
-            //               padding: const EdgeInsets.only(top: 8),
-            //               child: Text("${snap.data.email}"),
-            //             ),
-            //           ],
-            //         ),
-            //       ),
-            //     );
-            //   },
-            // ),
-            // /
-
             StreamBuilder<UsuarioModel>(
-                stream: authBloc.perfil,
-                builder: (BuildContext context,
-                AsyncSnapshot<UsuarioModel> snap) {
-                  if (snap.hasError) {
-                    return Center(
-                      child: Text("Erro"),
-                    );
-                  }
-                  if (!snap.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  List<Widget> list = List<Widget>();
-                  if (snap.data == null || snap.data.routes == null ||
-                      snap.data.routes.isEmpty) {
-                    list.add(Container(
-                      height: 50,
-                      color: Colors.green,
-                    ));
-                  } else {
-                    rotas.forEach((k, v) {
-                      if (snap.data.routes.contains(k)) {
-                        list.add(ListTile(
-                          title: Text(v.nome),
-                          trailing: Icon(v.Icons),
-                          onTap: () {
-                            Navigator.pushReplacementNamed(context, k);
-                          },
-                        ));
-                      }
-                    });
-                  }
-                  if (list.isEmpty || list == null) {
-                    list.add(Container(color: Colors.red));
-                  }
-                  return Expanded(child: ListView(children: list));
+              stream: authBloc.perfil,
+              builder: (context, snap) {
+                if (snap.hasError) {
+                  return Center(
+                    child: Text("Erro"),
+                  );
+                }
+                if (!snap.hasData)
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                Widget imagem = Icon(Icons.people, size: 75);
+                if (snap.data?.foto?.localPath != null) {
+                  imagem = Container(
+                      color: Colors.yellow,
+                      child: Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: CircleAvatar(
+                            backgroundImage:
+                                ExactAssetImage(snap.data?.foto?.localPath),
+                            minRadius: 50,
+                            maxRadius: 50,
+                          )));
+                } else if (snap.data?.foto?.url != null) {
+                  imagem = CircleAvatar(
+                    backgroundImage: NetworkImage(snap.data?.foto?.url),
+                    minRadius: 50,
+                    maxRadius: 50,
+                  );
+                }
+                return DrawerHeader(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                            color: Theme.of(context).textTheme.title.color),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(flex: 4, child: imagem),
+                            Expanded(
+                              flex: 8,
+                              child: Container(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 8),
+                                      child: Text("${snap.data.nome}"),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 8),
+                                      child: Text("${snap.data.celular}"),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8),
+                          child: Text("${snap.data.email}"),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            StreamBuilder<UsuarioModel>(
+              stream: authBloc.perfil,
+              builder:
+                  (BuildContext context, AsyncSnapshot<UsuarioModel> snap) {
+                if (snap.hasError) {
+                  return Center(
+                    child: Text("Erro"),
+                  );
+                }
+                if (!snap.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                List<Widget> list = List<Widget>();
+                if (snap.data == null ||
+                    snap.data.routes == null ||
+                    snap.data.routes.isEmpty) {
+                  list.add(Container(
+                    height: 50,
+                    color: Colors.green,
+                  ));
+                } else {
+                  rotas.forEach((k, v) {
+                    if (snap.data.routes.contains(k)) {
+                      list.add(ListTile(
+                        title: Text(v.nome),
+                        trailing: Icon(v.Icons),
+                        onTap: () {
+                          Navigator.pushReplacementNamed(context, k);
+                        },
+                      ));
+                    }
+                  });
+                }
+                if (list.isEmpty || list == null) {
+                  list.add(Container(color: Colors.red));
+                }
+                return Expanded(child: ListView(children: list));
 
-                  // return ListTile(
-                  //         title: Text("v.nome"),
-                  //         trailing: Icon(Icons.ac_unit),
-                  //         onTap: () {
-                  //           //Navigator.pushReplacementNamed(context, k);
-                  //         },
-                  //       );
-                })
-          ]),
-    ));
+                // return ListTile(
+                //         title: Text("v.nome"),
+                //         trailing: Icon(Icons.ac_unit),
+                //         onTap: () {
+                //           //Navigator.pushReplacementNamed(context, k);
+                //         },
+                //       );
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -257,16 +257,15 @@ class DefaultScaffold extends StatelessWidget {
   final Widget actions;
   final Widget bottom;
   final Color backgroundColor;
-
-  const DefaultScaffold(
-      {Key key,
-      this.body,
-      this.floatingActionButton,
-      this.title,
-      this.actions,
-      this.backgroundColor,
-      this.bottom})
-      : super(key: key);
+  const DefaultScaffold({
+    Key key,
+    this.body,
+    this.floatingActionButton,
+    this.title,
+    this.actions,
+    this.backgroundColor,
+    this.bottom,
+  }) : super(key: key);
 
   Widget _appBarBuild(BuildContext context) {
     return AppBar(
